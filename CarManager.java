9b025db9a16287c220e2943a5bdd47bff7c3d804
pavdem2012 <pavdem2012@gmail.com>
2021-02-18 01:44:57 +1100

    class instances (objects)

--- CarManager.java
+++ CarManager.java
@@ -2,8 +2,30 @@ package com.pavdem2012.carmanager;
 
 public class CarManager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+    	
+        Car prius = new Car();
+        
+        prius.name = "Toyota Prius";
+        
+        prius.color = "Green";
+        
+        prius.yearOfProdaction = 2008;
+        
+        prius.price = 15000;
+        
+        prius.weight = 1200;
+        
+        Car renault = new Car();
+        renault.name = "Renault Laguna";
+        
+        renault.color = "Wite";
+        
+        renault.yearOfProdaction = 2000;
+        
+        renault.price = 9700;
+        
+        renault.weight = 1600;
 
 	}
 
