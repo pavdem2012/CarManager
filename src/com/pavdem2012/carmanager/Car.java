1dd77975cb51374703f0a46d452c30dd180c84fb
pavdem2012 <pavdem2012@gmail.com>
2021-02-18 22:37:50 +1100

    add equals

--- src/com/pavdem2012/carmanager/Car.java
+++ src/com/pavdem2012/carmanager/Car.java
@@ -21,4 +21,42 @@ public class Car {
                 + weight + ", color=" + color + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((color == null) ? 0 : color.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + price;
+        result = prime * result + weight;
+        result = prime * result + yearOfProdaction;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Car other = (Car) obj;
+        if (color != other.color)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (price != other.price)
+            return false;
+        if (weight != other.weight)
+            return false;
+        if (yearOfProdaction != other.yearOfProdaction)
+            return false;
+        return true;
+    }
+    
+
 }
--- src/com/pavdem2012/carmanager/CarManager.java
+++ src/com/pavdem2012/carmanager/CarManager.java
@@ -7,10 +7,15 @@ public class CarManager {
         Car prius = new Car("Toyota Prius", 2008, 15000, 1200, "Green");
 
         Car renault = new Car("Renault Laguna", 2000, 9700, 1600, "White");
+        
+        Car renault2 = new Car("Renault Laguna", 2000, 9700, 1600, "White");
 
         System.out.println(prius);
-
         System.out.println(renault);
+
+        System.out.println(renault.equals(renault2));
+        System.out.println(renault.equals(prius));
+ 
     }
 
 }
